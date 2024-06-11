aea868fec86e16fc9522874ab96817516bfff158
Chaoming Wang <adaduo@outlook.com>
2024-06-11 22:15:01 +0800

    rewrite ``_make_jaxpr()`` function to be compatible with ``jax==0.4.29`` (#3)
    * rewrite ``_make_jaxpr()`` function to be compatible with ``jax==0.4.29``
    
    * fix typing annotation error in python<=3.9

--- brainstate/functional/__init__.py
+++ brainstate/functional/__init__.py
@@ -20,6 +20,7 @@ from ._normalization import *
 from ._normalization import __all__ as __others_all__
 from ._spikes import *
 from ._spikes import __all__ as __spikes_all__
+from ._others import *
+from ._others import __all__ as __others_all__
 
-__all__ = __spikes_all__ + __others_all__ + __activations_all__
-
+__all__ = __spikes_all__ + __others_all__ + __activations_all__ + __others_all__
