7bd9aa33e3e70b3bf4c486eb3ce6f423d4a8c766
Chaoming Wang <adaduo@outlook.com>
2024-08-10 14:04:00 +0800

    upgrade to support `brainunit>=0.0.2`

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
   author_email='chao.brain@qq.com',
   packages=packages,
   python_requires='>=3.9',
-  install_requires=['numpy>=1.15', 'jax', 'tqdm', 'brainunit'],
+  install_requires=['numpy>=1.15', 'jax', 'tqdm', 'brainunit>=0.0.2'],
   url='https://github.com/brainpy/brainstate',
   project_urls={
     "Bug Tracker": "https://github.com/brainpy/brainstate/issues",
