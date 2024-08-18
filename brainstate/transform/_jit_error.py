1673d6f30950679c66e38bed9279abf0d3243b1e
Chaoming Wang <adaduo@outlook.com>
2024-08-18 14:05:13 +0800

    add `brainstate.transform.map()` and `brainstate.transform.bounded_while_loop()`

--- brainstate/transform/_jit_error.py
+++ brainstate/transform/_jit_error.py
@@ -20,99 +20,13 @@ from functools import partial
 from typing import Callable, Union
 
 import jax
-from jax import numpy as jnp
-from jax.core import Primitive, ShapedArray
-from jax.interpreters import batching, mlir
 
 from brainstate._utils import set_module_as
+from ._unvmap import remove_vmap
 
 __all__ = [
   'jit_error',
 ]
-
-
-@set_module_as('brainstate.transform')
-def remove_vmap(x, op: str = 'any'):
-  if op == 'any':
-    return _any_without_vmap(x)
-  elif op == 'all':
-    return _all_without_vmap(x)
-  elif op == 'none':
-    return _without_vmap(x)
-  else:
-    raise ValueError(f'Do not support type: {op}')
-
-
-def _without_vmap(x):
-  return _no_vmap_prim.bind(x)
-
-
-def _without_vmap_imp(x):
-  return x
-
-
-def _without_vmap_abs(x):
-  return x
-
-
-def _without_vmap_batch(x, batch_axes):
-  (x,) = x
-  return _without_vmap(x), batching.not_mapped
-
-
-_no_vmap_prim = Primitive('no_vmap')
-_no_vmap_prim.def_impl(_without_vmap_imp)
-_no_vmap_prim.def_abstract_eval(_without_vmap_abs)
-batching.primitive_batchers[_no_vmap_prim] = _without_vmap_batch
-mlir.register_lowering(_no_vmap_prim, mlir.lower_fun(_without_vmap_imp, multiple_results=False))
-
-
-def _any_without_vmap(x):
-  return _any_no_vmap_prim.bind(x)
-
-
-def _any_without_vmap_imp(x):
-  return jnp.any(x)
-
-
-def _any_without_vmap_abs(x):
-  return ShapedArray(shape=(), dtype=jnp.bool_)
-
-
-def _any_without_vmap_batch(x, batch_axes):
-  (x,) = x
-  return _any_without_vmap(x), batching.not_mapped
-
-
-_any_no_vmap_prim = Primitive('any_no_vmap')
-_any_no_vmap_prim.def_impl(_any_without_vmap_imp)
-_any_no_vmap_prim.def_abstract_eval(_any_without_vmap_abs)
-batching.primitive_batchers[_any_no_vmap_prim] = _any_without_vmap_batch
-mlir.register_lowering(_any_no_vmap_prim, mlir.lower_fun(_any_without_vmap_imp, multiple_results=False))
-
-
-def _all_without_vmap(x):
-  return _all_no_vmap_prim.bind(x)
-
-
-def _all_without_vmap_imp(x):
-  return jnp.all(x)
-
-
-def _all_without_vmap_abs(x):
-  return ShapedArray(shape=(), dtype=jnp.bool_)
-
-
-def _all_without_vmap_batch(x, batch_axes):
-  (x,) = x
-  return _all_without_vmap(x), batching.not_mapped
-
-
-_all_no_vmap_prim = Primitive('all_no_vmap')
-_all_no_vmap_prim.def_impl(_all_without_vmap_imp)
-_all_no_vmap_prim.def_abstract_eval(_all_without_vmap_abs)
-batching.primitive_batchers[_all_no_vmap_prim] = _all_without_vmap_batch
-mlir.register_lowering(_all_no_vmap_prim, mlir.lower_fun(_all_without_vmap_imp, multiple_results=False))
 
 
 def _err_jit_true_branch(err_fun, args, kwargs):
@@ -134,7 +48,7 @@ def _error_msg(msg, *arg, **kwargs):
 @set_module_as('brainstate.transform')
 def jit_error(
     pred,
-    err_fun: Union[Callable, str],
+    error: Union[Callable, str],
     *err_args,
     **err_kwargs,
 ):
@@ -161,19 +75,19 @@ def jit_error(
   ----------
   pred: bool, Array
     The boolean prediction.
-  err_fun: callable
-    The error function, which raise errors.
+  error: callable, str
+    The error function, which raise errors, or a string indicating the error message.
   err_args: 
     The arguments which passed into `err_f`.
   err_kwargs: 
     The keywords which passed into `err_f`.
   """
-  if isinstance(err_fun, str):
-    err_fun = partial(_error_msg, err_fun)
+  if isinstance(error, str):
+    error = partial(_error_msg, error)
 
   jax.lax.cond(
     remove_vmap(pred, op='any'),
-    partial(_err_jit_true_branch, err_fun),
+    partial(_err_jit_true_branch, error),
     _err_jit_false_branch,
     jax.tree.map(functools.partial(remove_vmap, op='none'), err_args),
     jax.tree.map(functools.partial(remove_vmap, op='none'), err_kwargs),
