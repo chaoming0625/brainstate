ab438d73634df4306226e80b58b27bf027687594
Chaoming Wang <adaduo@outlook.com>
2024-09-29 11:38:23 +0800

    add `OptaxOptimizer` class

--- brainstate/optim/__init__.py
+++ brainstate/optim/__init__.py
@@ -16,7 +16,11 @@
 
 from ._lr_scheduler import *
 from ._lr_scheduler import __all__ as scheduler_all
+from ._optax_optimizer import *
+from ._optax_optimizer import __all__ as optax_all
 from ._sgd_optimizer import *
 from ._sgd_optimizer import __all__ as optimizer_all
 
-__all__ = scheduler_all + optimizer_all
+__all__ = scheduler_all + optimizer_all + optax_all
+
+del optax_all, scheduler_all, optimizer_all
